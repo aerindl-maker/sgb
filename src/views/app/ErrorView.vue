45264ab085be2026623ad37d90dc2e9a4f9557c7
i4nizer <iansandoval264@gmail.com>
2026-07-03 21:59:17 +0800

    Redesigned error cards in error page.

--- src/views/app/ErrorView.vue
+++ src/views/app/ErrorView.vue
@@ -7,7 +7,7 @@
 			</v-col>
 		</v-row>
 		<v-row dense align="center">
-			<v-col cols="12" sm="5">
+			<v-col cols="6" lg="12">
 				<v-date-input
 					v-model="alpha"
 					label="From"
@@ -15,32 +15,17 @@
 					prepend-icon="mdi-calendar-start"
 				></v-date-input>
 			</v-col>
-			<v-col cols="12" sm="5">
-				<v-date-input v-model="omega" label="To" color="accent" prepend-icon="mdi-calendar-end"></v-date-input>
-			</v-col>
-			<v-col cols="12" sm="2" class="d-flex align-center ga-2">
-				<v-btn icon="mdi-calendar-today" color="accent" variant="tonal" @click="onClickToday"></v-btn>
-				<v-btn
-					icon="mdi-filter"
-					color="accent"
-					:disabled="hasDateError"
-					:loading="isFetchingFaults"
-					@click="getFaults"
-				></v-btn>
-			</v-col>
-		</v-row>
-		<v-row v-if="hasDateError" dense>
-			<v-col cols="12">
-				<v-alert
-					type="error"
-					variant="tonal"
-					density="compact"
-					text="Start date must be before end date."
-				></v-alert>
+			<v-col cols="6" lg="12">
+				<v-date-input 
+					v-model="omega" 
+					label="To" 
+					color="accent" 
+					prepend-icon="mdi-calendar-end"
+				></v-date-input>
 			</v-col>
 		</v-row>
 		<v-row dense>
-			<v-col v-if="isFetchingFaults" v-for="n in [1, 2, 3]" :key="n" cols="12" md="6" lg="4">
+			<v-col v-if="isFetchingFaults" v-for="n in [1, 2, 3, 4, 5]" :key="n" cols="12" md="6" lg="4">
 				<v-skeleton-loader type="article"></v-skeleton-loader>
 			</v-col>
 			<v-col v-else-if="!faults.length" cols="12">
@@ -53,25 +38,18 @@
 			</v-col>
 			<v-col v-else v-for="fault in faults" :key="fault.id" cols="12" md="6" lg="4">
 				<v-card elevation="1">
-					<template #prepend>
-						<v-avatar color="error" variant="tonal">
-							<v-icon icon="mdi-alert-circle-outline"></v-icon>
-						</v-avatar>
-					</template>
-					<template #title>
-						<span>{{ fault.title }}</span>
-					</template>
-					<template #subtitle>
-						<span>{{ dateCmp.format(fault.createdAt, "fullDateTime12h") }}</span>
-					</template>
-					<template #text>
-						<div class="d-flex flex-column ga-3">
-							<span>{{ fault.message }}</span>
-							<v-chip size="small" color="error" variant="tonal" prepend-icon="mdi-alert">
-								Sensor Fault
-							</v-chip>
+					<v-card-text class="d-flex">
+						<div class="d-flex ga-2 align-center flex-grow-1">
+							<v-avatar color="error" variant="tonal">
+								<v-icon icon="mdi-alert-circle-outline"></v-icon>
+							</v-avatar>
+							<div>
+								<div>{{ fault.title }}</div>
+								<div style="font-size: x-small" class="text-grey">{{ fault.message }}</div>
+							</div>
 						</div>
-					</template>
+						<div style="font-size: xx-small" class="text-grey">{{ dateCmp.format(fault.createdAt, "keyboardDateTime12h") }}</div>
+					</v-card-text>
 				</v-card>
 			</v-col>
 		</v-row>
@@ -102,9 +80,8 @@ const createTodayRange = () => {
 }
 
 const defaultRange = createTodayRange()
-const alpha = ref<Date | null>(defaultRange.alpha)
-const omega = ref<Date | null>(defaultRange.omega)
-const hasDateError = computed(() => !!alpha.value && !!omega.value && alpha.value.getTime() > omega.value.getTime())
+const alpha = ref<Date>(defaultRange.alpha)
+const omega = ref<Date>(defaultRange.omega)
 
 const createFaultParams = () => ({
 	limit: 100,
@@ -129,7 +106,8 @@ const faults = ref<FaultSchema[]>([])
 const isFetchingFaults = ref(false)
 
 const getFaults = async () => {
-	if (hasDateError.value) return toastCmp.error("Invalid date range.")
+	const invalid = alpha.value.getTime() > omega.value.getTime()
+	if (invalid) [alpha.value, omega.value] = [omega.value, alpha.value];
 
 	isFetchingFaults.value = true
 	await api
@@ -138,13 +116,6 @@ const getFaults = async () => {
 		.then(data => (faults.value = data))
 		.catch(() => toastCmp.error("Something went wrong."))
 		.finally(() => (isFetchingFaults.value = false))
-}
-
-const onClickToday = async () => {
-	const range = createTodayRange()
-	alpha.value = range.alpha
-	omega.value = range.omega
-	await getFaults()
 }
 
 //
